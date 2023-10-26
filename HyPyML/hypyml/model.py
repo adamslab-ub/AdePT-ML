8caffb6302cde27b5985cdf19f78f6f9dd8a5d4c
Manaswin Oddiraju <moddiraj@buffalo.edu>
2023-10-26 02:08:49 -0400

    Version 0.1.4 Features: - working serial architectures - updated Readme - updated and working sample_train

--- HyPyML/hypyml/model.py
+++ HyPyML/hypyml/model.py
@@ -3,13 +3,14 @@ import torch.nn
 import numpy as np
 from joblib import dump
 import os
+from hypyml.sub_models import MLP, Physics, CNN
     
-class OPTMA(torch.nn.Module): 
+class Torch_Wrapper(torch.nn.Module): 
     """
     Architecture is a list of dicts containing the following fields: Model type: "Physics" or "MLP"
     """
     def __init__(self,architecture):
-        super(OPTMA, self).__init__()
+        super(Torch_Wrapper, self).__init__()
         self.models_mlp=torch.nn.ModuleList()
         self.models_cnn=torch.nn.ModuleList()
         self.models_physics = []
@@ -29,47 +30,6 @@ class OPTMA(torch.nn.Module):
                 self.model_ref.append(['CNN', len(self.models_cnn)-1])
             self.args.append(i["args"])
     
-    def fwd_till(self,x,end_block):
-        return self.fwd_selective(x,start_block=0, end_block=end_block)
-    
-    def fwd_from(self,x,start_block):
-        return self.fwd_selective(x,start_block=start_block,end_block=len(self.arch)-1)
-    
-    def fwd_selective(self,x,start_block,end_block):
-        """
-        Runs inference from any specified start module in architecture to any end point
-        """
-        out = x
-        phy_count = 0
-        mlp_count = 0
-        cnn_count = 0
-        
-        assert end_block < len(self.arch), "Max possible value of end_block is %d, but recieved %d" %(len(self.arch)-1, end_block)
-
-        for i in range(start_block, end_block): 
-            ## Choosing the correct model from list
-            count = self.model_ref[i][0]
-            if self.arch[i]["Type"] == "Physics":
-                model = self.models_physics[count]
-                if self.args[i] != None:
-                    out = model(out,self.arch[i]['Forward'],self.arch[i]['Jacobian'], x[:,self.args[i]])
-                else: 
-                    out = model(out,self.arch[i]['Forward'],self.arch[i]['Jacobian'])
-            elif self.arch[i]["Type"] == "CNN" : 
-                model = self.models_cnn[count]
-                if self.args[i] != None:
-                    out = model(torch.hstack((x[:,self.args[i]],out)))
-                else:
-                    out = model(out)
-            else: 
-                model = self.models_mlp[count]
-                if self.args[i] != None:
-                    out = model(torch.hstack((x[:,self.args[i]],out)))
-                else:
-                    out = model(out)
-        return out
-
-    # @staticmethod
     def forward(self,x):
         out = x
         phy_count = 0
@@ -102,13 +62,60 @@ class OPTMA(torch.nn.Module):
                 else:
                     out = model(out)
         return out
- 
+    
+class HyPyML():
+    def __init__(self,architecture):
+        self.optma = Torch_Wrapper(architecture)
+    
+    def forward(self,x):
+        return self.optma.forward(x)
 
-    def train_step(model,optimizer,loss_fn,scheduler=None ):
+    def fwd_till(self,x,end_block):
+        return self.fwd_selective(x,start_block=0, end_block=end_block)
+    
+    def fwd_from(self,x,start_block):
+        return self.fwd_selective(x,start_block=start_block,end_block=len(self.optma.arch)-1)
+    
+    def fwd_selective(self,x,start_block,end_block):
+        """
+        Runs inference from any specified start module in architecture to any end point
+        """
+        out = x
+        phy_count = 0
+        mlp_count = 0
+        cnn_count = 0
+        
+        assert end_block < len(self.optma.arch), "Max possible value of end_block is %d, but recieved %d" %(len(self.optma.arch)-1, end_block)
+
+        for i in range(start_block, end_block): 
+            ## Choosing the correct model from list
+            count = self.optma.model_ref[i][0]
+            if self.optma.arch[i]["Type"] == "Physics":
+                tmp_model = self.optma.models_physics[count]
+                if self.args[i] != None:
+                    out = tmp_model(out,self.optma.arch[i]['Forward'],self.optma.arch[i]['Jacobian'], x[:,self.args[i]])
+                else: 
+                    out = tmp_model(out,self.optma.arch[i]['Forward'],self.optma.arch[i]['Jacobian'])
+            elif self.optma.arch[i]["Type"] == "CNN" : 
+                tmp_model = self.optma.models_cnn[count]
+                if self.args[i] != None:
+                    out = tmp_model(torch.hstack((x[:,self.args[i]],out)))
+                else:
+                    out = tmp_model(out)
+            else: 
+                tmp_model = self.optma.models_mlp[count]
+                if self.args[i] != None:
+                    out = tmp_model(torch.hstack((x[:,self.args[i]],out)))
+                else:
+                    out = tmp_model(out)
+        return out
+
+    # @staticmethod
+    def train_step(self,optimizer,loss_fn,scheduler=None ):
         # Builds function that performs a step in the train loop
         def train_step(x, y,test=False):
             if not test:
-                yhat = model.forward(x)
+                yhat = self.forward(x)
                 loss = loss_fn(yhat,y)#torch.mean(torch.abs(yhat-y))
                 optimizer.zero_grad()
                 loss.backward()
@@ -116,7 +123,7 @@ class OPTMA(torch.nn.Module):
                 if scheduler:
                     scheduler.step()
             else:
-                a = model.eval()
+                a = self.optma.eval()
                 with torch.no_grad():
                     yhat = a(x)
                     loss = loss_fn(yhat,y) 
@@ -124,11 +131,12 @@ class OPTMA(torch.nn.Module):
         # Returns the function that will be called inside the train loop
         return train_step
 
-    def train_model(train_loader, test_loader, model, optimizer, scheduler, loss_fn, filename, epochs):
+    def train_model(self,train_loader, test_loader, optimizer, loss_fn, scheduler, filename, epochs):
+
         data_dir = os.path.join(os.getcwd(),"Models/Training_History_%s" % filename)
         if not os.path.exists(data_dir): 
             os.system("mkdir %s" % data_dir)
-        train_step_obj = train_step(model,optimizer, loss_fn,scheduler)
+        train_step_obj = self.train_step(optimizer, loss_fn,scheduler)
         training_loss = []
         test_loss =[]
         for epoch in range(epochs):
@@ -144,10 +152,9 @@ class OPTMA(torch.nn.Module):
             test_loss.append(np.mean(batch_losses))
             print('epoch',epoch,'training loss',training_loss[-1], 'test loss',test_loss[-1])
             if epoch%50 ==0:
-                torch.save(model.state_dict(), "%s/Model_%d.pt" %(data_dir,epoch))
-        torch.save(model.state_dict(), data_dir+"/Model_final.pt")
-
-        tmp = model.arch
+                torch.save(self.optma.state_dict(), "%s/Model_%d.pt" %(data_dir,epoch))
+        torch.save(self.optma.state_dict(), data_dir+"/Model_final.pt")
+        tmp = self.optma.arch
         reduced_arch =[]
         for i in tmp: 
             if i["Type"] == "Physics": 
@@ -156,4 +163,4 @@ class OPTMA(torch.nn.Module):
                 reduced_arch.append(i)
         dump(dict(train_loss=training_loss, test_loss=test_loss, model_architecture = reduced_arch),"%s/Training_History.joblib" % data_dir)
 
-        return model,training_loss, test_loss
+        return training_loss, test_loss
--- sample_train.py
+++ sample_train.py
@@ -1,41 +1,62 @@
 import numpy as np
+import torch
 import torch.utils.data
 # import matplotlib.pyplot as plt
-import time
-from hypyml.OPTMA import *
-import config1 as c
-c.filename = 'No_Args'
-trainset = np.load(c.trainset)
-testset = np.load(c.testset)
-output=[]
-error = []
-xtest = torch.Tensor(testset[:,:c.D_in])
-ytest = torch.Tensor(testset[:,c.D_in:])
-xtrain = torch.Tensor(trainset[:,:c.D_in])
-ytrain = torch.Tensor(trainset[:,c.D_in:])
-x_train = torch.Tensor(xtrain).to(c.device)
-y_train = torch.Tensor(ytrain).to(c.device)
-x_test= torch.Tensor(xtest).to(c.device)
-y_test = torch.Tensor(ytest).to(c.device)
+from Part_Physics import batch_physics, batch_jacobian
+import plotly.graph_objects as go
+from hypyml.model import HyPyML
 
+#%% Setting up model config
+filename = 'Trial_Run'
+trainset = np.load("train_data.npy")
+testset = np.load("test_data.npy")
+D_in = 1
+D_PP = 1
+D_out = 1
+batch_size = 10
+lr = 1e-4
+epochs = 300
+
+
+#%% Loading Data
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+xtest = torch.Tensor(testset[:,:D_in])
+ytest = torch.Tensor(testset[:,D_in:])
+xtrain = torch.Tensor(trainset[:,:D_in])
+ytrain = torch.Tensor(trainset[:,D_in:])
+x_train = xtrain.to(device)
+y_train = ytrain.to(device)
+x_test= xtest.to(device)
+y_test = ytest.to(device)
 test_loader = torch.utils.data.DataLoader(
     torch.utils.data.TensorDataset(x_test, y_test),
-    batch_size=17, shuffle=True, drop_last=True)
-
+    batch_size=100, shuffle=True, drop_last=True)
 train_loader = torch.utils.data.DataLoader(
     torch.utils.data.TensorDataset(x_train, y_train),
-    batch_size=c.batch_size, shuffle=True, drop_last=True)
+    batch_size=batch_size, shuffle=True, drop_last=True)
 #%% Initalize model and optimizer
+
 # If MLP, dict should have the following dict config=dict(D_in= , D_out= , Hidden_layer_size= , Num_layers= )
 # If Physics, dict should have the following fields "Forward", "Jacobian"
-architecture = [dict(Type="MLP",config=dict(D_in=c.D_in , D_out= c.D_PP, Hidden_layer_size= 100, Num_layers= 3),args =None),
-                dict(Type="Physics", Forward = c.finite_diff_simple, Jacobian = c.partial_jacobian,args = None),
-                dict(Type="Physics", Forward = c.post_process, Jacobian = c.partial_jacobian,args = None),]
 
+architecture = [dict(Type="Physics", Forward = batch_physics, Jacobian = batch_jacobian,args = None), 
+                dict(Type="MLP",config=dict(D_in=D_in , D_out= D_PP, Hidden_layer_size= 50, Num_layers= 2),args =None)]
 
-model = OPTMA(architecture)
-model.to(c.device)
-optimizer = torch.optim.Adam(model.parameters(),c.lr,weight_decay=c.weight_decay)
+model = HyPyML(architecture)
+model.optma.to(device)
+optimizer = torch.optim.Adam(model.optma.parameters(),lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer,step_size=30 ,gamma=0.95)
 loss_fn = torch.nn.MSELoss()
-model, train_loss, test_loss = train_model(train_loader, test_loader, model, optimizer, scheduler, loss_fn, c.filename, c.epochs)
+
+#%% Train
+train_loss, test_loss = model.train_model(train_loader, test_loader, optimizer, loss_fn,scheduler, filename, epochs)
+
+#%% Testing Model and Generating Plots
+predictions = model.forward(x_test)
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=xtest.detach().numpy().ravel(), y = predictions.detach().cpu().numpy().ravel(),mode ="markers", name="PIML Predictions"))
+fig.add_trace(go.Scatter(x=xtest.detach().numpy().ravel(), y = ytest.detach().numpy().ravel(),mode ="markers", name="Truth"))
+fig.show()
