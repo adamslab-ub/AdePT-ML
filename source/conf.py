62413adedc09bc81b567ee2ece992d36914de837
Manaswin Oddiraju <moddiraj@buffalo.edu>
2026-03-14 17:05:23 -0600

    Build docs, fix RTD config to install from source, bump version to 2.0

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Manaswin Oddiraju"
 author = "Manaswin Oddiraju"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0"
+release = "2.0"
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
